8bd3ee119adb14402665552e2b2bc7844f638ac1
danix <danish.a.chaudhry@gmail.com>
2015-03-08 01:30:26 +0500

    test fire

--- src/main/java/eu/pick/initializer/AppInitializer.java
+++ src/main/java/eu/pick/initializer/AppInitializer.java
@@ -3,52 +3,38 @@ package eu.pick.initializer;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class AppInitializer implements WebApplicationInitializer {
 
+  //  private static final String CONFIG_LOCATION = "eu.pick.config";
     private static final String MAPPING_URL = "/*";
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-    	AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-    	rootContext.register(eu.pick.config.AppConfig.class);
-    	rootContext.setDisplayName("SoPickmeUP");
     	
-   // Add context loader listener 
-    	servletContext.addListener(new ContextLoaderListener(rootContext));
-    	
-/*        WebApplicationContext context = getContext();
-        servletContext.addListener(new ContextLoaderListener(context));*/
-    	
-    	
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(rootContext));
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping(MAPPING_URL);
         
-		// Register Spring security filter
-		FilterRegistration.Dynamic springSecurityFilterChain = 
-				servletContext.addFilter("springSecurityFilterChain", DelegatingFilterProxy.class);
-		springSecurityFilterChain.addMappingForUrlPatterns(null, false, "/*");
-		
-		// Register Spring Social filter so that we can disconnect from providers
-		FilterRegistration.Dynamic hiddenHttpMethodFilter = 
-				servletContext.addFilter("hiddenHttpMethodFilter", HiddenHttpMethodFilter.class);
-		hiddenHttpMethodFilter.addMappingForUrlPatterns(null, false, "/*");
     }
 
-/*    private AnnotationConfigWebApplicationContext getContext() {
+    private AnnotationConfigWebApplicationContext getContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.setConfigLocation(CONFIG_LOCATION);
-        return context;
-    }*/
+        
+        context.register(eu.pick.config.AppConfig.class);
+        
+      //  context.setConfigLocation(CONFIG_LOCATION);
+        context.setDisplayName("SoPickMeUp");
+               return context;
+    }
 
 }
