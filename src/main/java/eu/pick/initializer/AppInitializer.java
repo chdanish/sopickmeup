1509a37826c4730ed97563df8edfd779b5e882e8
danix <danish.a.chaudhry@gmail.com>
2015-03-08 01:38:47 +0500

    set app listening path

--- src/main/java/eu/pick/initializer/AppInitializer.java
+++ src/main/java/eu/pick/initializer/AppInitializer.java
@@ -5,8 +5,11 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -14,7 +17,7 @@ import javax.servlet.ServletRegistration;
 public class AppInitializer implements WebApplicationInitializer {
 
   //  private static final String CONFIG_LOCATION = "eu.pick.config";
-    private static final String MAPPING_URL = "/*";
+    private static final String MAPPING_URL = "/";
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -24,6 +27,17 @@ public class AppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping(MAPPING_URL);
+        
+        
+     // Register Spring security filter
+     		FilterRegistration.Dynamic springSecurityFilterChain = 
+     				servletContext.addFilter("springSecurityFilterChain", DelegatingFilterProxy.class);
+     		springSecurityFilterChain.addMappingForUrlPatterns(null, false, "/*");
+     		
+     		// Register Spring Social filter so that we can disconnect from providers
+     		FilterRegistration.Dynamic hiddenHttpMethodFilter = 
+     				servletContext.addFilter("hiddenHttpMethodFilter", HiddenHttpMethodFilter.class);
+     		hiddenHttpMethodFilter.addMappingForUrlPatterns(null, false, "/*");
         
     }
 
